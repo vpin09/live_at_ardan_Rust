e139fbce9ddce1e41b6d5d59faa79568fae536e9
“vpin09” <vpin.saini@techalchemy.com>
2025-04-09 16:18:58 +0530

    login

--- session1/hello-world/src/main.rs
+++ session1/hello-world/src/main.rs
@@ -1,3 +1,32 @@
+use authentication::{greet,read_line,login};
 fn main() {
-    println!("Hello, world!");
+    println!("{}", greet("Vipin"));
+
+
+    let mut tries:i8=0;
+    loop {
+        println!("please enter username ");
+        let mut username=read_line();
+        println!("please enter password");
+        let mut password=read_line();
+
+        if login(&username, &password) {
+
+            println!("you have been succesfully logined");
+            break;
+
+        }
+
+        tries += 1;
+        if tries >= 3 {
+            println!("login attempt limit exceed");
+            break;
+        }
+
+        println!("Please try again : Inccorect  username or password");
+        println!("login attemp left {}", 3-tries);
+
+    }
+
+
 }
