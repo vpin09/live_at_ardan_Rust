575fca483260d20fcf20f05f5a461cb601954c0c
“vpin09” <vpin.saini@techalchemy.com>
2025-04-11 10:55:52 +0530

    hashing

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -1,3 +1,6 @@
+use std::{ collections::HashMap, path::Path };
+use serde::{ Serialize, Deserialize };
+
 pub fn greet(name: &str) -> String {
     format!("Hello {}", name)
 }
@@ -7,12 +10,13 @@ pub enum LoginAction {
     Granted(LoginRole),
     Denied,
 }
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     username: String,
     password: String,
@@ -22,29 +26,69 @@ impl User {
     pub fn new(username: &str, password: &str, role: LoginRole) -> Self {
         User {
             username: username.to_lowercase(),
-            password: password.to_string(),
+            password: hash_passowrd(password),
             role,
         }
     }
 }
 
-pub fn get_users() -> [User; 2] {
-    [
-        User::new("admin", "password", LoginRole::Admin),
-        User::new("bob", "password", LoginRole::User),
-    ]
+pub fn hash_passowrd(password: &str) -> String {
+    use sha2::Digest;
+
+    let mut hasher=sha2::Sha256::new();
+    hasher.update(password);
+    format!("{:X}",hasher.finalize())
 }
+/*pub fn get_users() -> Vec<User> {
+    vec![
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("bob", "password", LoginRole::User)
+    ]
+}*/
+
+pub fn get_default_users() -> HashMap<String, User> {
+    let mut users = HashMap::new();
+    users.insert("admin".to_string(), User::new("admin", "password", LoginRole::Admin));
+
+    users.insert("bob".to_string(), User::new("bob", "password", LoginRole::User));
+    users
+}
+
+pub fn get_users() -> HashMap<String, User> {
+    let user_path = Path::new("user.json");
+    if user_path.exists() {
+        let user_json = std::fs::read_to_string(user_path).unwrap();
+        let user: HashMap<String, User> = serde_json::from_str(&user_json).unwrap();
+        user
+    } else {
+        let user = get_default_users();
+        let user_json = serde_json::to_string(&user).unwrap();
+        std::fs::write(user_path, user_json);
+        user
+    }
+}
+
+/* 
+pub fn get_admin_user() {
+    let users: Vec<String> = get_users()
+        .into_iter()
+        .filter(|u| u.role == LoginRole::Admin)
+        .map(|u| u.username)
+        .collect();
+}*/
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
+    let password=hash_passowrd(password);
 
     let users = get_users();
-    if let Some(user) = users.iter().find(|user| user.username == username) {
+    if let Some(user) = users.get(&username) {
         if user.password == password {
             return Some(LoginAction::Granted(user.role.clone()));
         } else {
             return Some(LoginAction::Denied);
         }
     }
+
     None
 }
 
